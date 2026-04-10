371353be4e47e1230388cd1060a290961d7c783a
Daniel de Rauglaudre <daniel.roglo@free.fr>
2026-04-10 20:19:06 +0200

    -

--- graph_rt.ml
+++ graph_rt.ml
@@ -1140,12 +1140,12 @@ value action_mandel wid ev =
   | None -> () ]
 ;
 
-Rt.button_font.val := "-*-terminus-bold-r-*-18-*";
-Rt.title_font.val := "-*-terminus-bold-r-*-18-*";
-Rt.term_font.(0) := "-*-terminus-bold-r-*-18-*";
-Rt.term_font.(1) := "-*-terminus-bold-r-*-18-*";
-Rt.term_font.(2) := "-*-terminus-medium-o-*-18-*";
-Rt.term_font.(3) := "-*-terminus-bold-r-*-18-*";
+Rt.button_font.val := "-*-terminus-bold-r-*-20-*";
+Rt.title_font.val := "-*-terminus-bold-r-*-20-*";
+Rt.term_font.(0) := "-*-terminus-bold-r-*-20-*";
+Rt.term_font.(1) := "-*-terminus-bold-r-*-20-*";
+Rt.term_font.(2) := "-*-terminus-medium-o-*-20-*";
+Rt.term_font.(3) := "-*-terminus-bold-r-*-20-*";
 
 value x_init init_pos init_wid init_hei c_pal_def c_pal = do {
   let xd = rt_initialize "" in
